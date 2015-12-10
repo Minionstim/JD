544006756eac0bcee6cff947b66f12b865939319
back__kom <alihoupeng@163.com>
2015-12-10 21:33:58 +0800

    Update admin.php

--- admin.php
+++ admin.php
@@ -9,3 +9,14 @@ define("__ROOT__", "/JD");
 //显示错误信息
 define("APP_DEBUG",true);
 include_once './ThinkPHP/ThinkPHP.php';
+#评论表
+/*create table reviews
+(
+	id				int				auto_increment				primary key,
+	articleId		int,
+	foreign key (articleId) references newsArticles(articleId),
+	userName		varchar(100)	not null,
+	body			varchar(1000)	not null,
+	face			varchar(20)		not null,
+	dateandtime		timestamp		default current_timestamp
+);*/
